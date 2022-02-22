8bd77a45a245d95749130f9f49602d2c127991ba
Ayush <theblacksheep1130@gmail.com>
2022-02-22 12:16:35 +0530

    index.html

--- script.js
+++ script.js
@@ -1,66 +1,111 @@
 let player = 0;
 let computer = 0;
+let number = 0;
+
+let computerLast = '';
+let playerLast = '';
+
+function pInput(value) {
+    
+    if (number == 1){
+        reset (computerLast, playerLast);
+        number = 0;
+    }
+    
+    playerLast = value.path[0];
+    playerLast.classList.add("pClicked");
+    number = 1;
+    return game(value.path[0].id);
+}
+
+function reset (computerLast, playerLast) {
+    if (computerLast.classList)
+    computerLast.classList.remove("cClicked");
+    playerLast.classList.remove("pClicked");
+}
+
+document.getElementById('rock').addEventListener("click", pInput);
+document.getElementById('paper').addEventListener("click", pInput);
+document.getElementById('scissor').addEventListener("click", pInput);
 
 function computerPlay() { 
     let num = Math.floor(Math.random() * (4 - 1) + 1);
-    if (num == 1)
-    return "Rock";
-    else if (num == 2)
-    return "Paper";
-    else 
-    return "Scissor";
+    if (num == 1) {
+        computerLast = document.getElementById("cRock");
+        computerColor("Rock");
+        return "Rock";
+    }
+    else if (num == 2) {
+        computerLast = document.getElementById("cPaper");
+        computerColor("Paper");
+        return "Paper";
+    }
+    else {
+        computerLast = document.getElementById("cScissor");
+        computerColor("Scissor");
+        return "Scissor";
+    }
+}
+
+function computerColor(col) {
+    if (col == "Rock")
+        document.getElementById("cRock").classList.add("cClicked");
+    else if (col == "Paper")
+        document.getElementById("cPaper").classList.add("cClicked");
+    else if (col == "Scissor")
+        document.getElementById("cScissor").classList.add("cClicked");
+    else
+        return;
 }
 
 function playRound(playerSelection, computerSelection) {
     
+    let display = document.getElementById("result");
+
     if(playerSelection == computerSelection)
-        return ("Tie!");
+        display.textContent =  ("Tie!");
     else if ((playerSelection == "Rock" && computerSelection == "Scissor") 
         || (playerSelection == "Scissor" && computerSelection == "Paper") 
         || (playerSelection == "Paper" && computerSelection == "Rock"))
         {
             player++;
-            return ("You Won! " + playerSelection + " beats " + computerSelection);
+            display.textContent = ("You Won! " + playerSelection + " beats " + computerSelection);
         }
     else
         {
             computer++;
-            return ("You Lost! " + computerSelection + " beats " + playerSelection);   
+            display.textContent = ("You Lost! " + computerSelection + " beats " + playerSelection);   
         }
 }
 
-function game() {
+function game(numb) {
 
-    for (i = 0; i < 5; i++)
+    const computerSelection = computerPlay();
+    
+    let input = numb;
+    let low = input.toLocaleLowerCase();
+    let output = low.charAt(0).toUpperCase();
+
+    for (j = 1; j < low.length; j++)
     {
-        const computerSelection = computerPlay();
-        
-        let input = window.prompt("What will you choose?");
-        let low = input.toLocaleLowerCase();
-        let output = low.charAt(0).toUpperCase();
-
-        for (j = 1; j < low.length; j++)
-        {
-            output += low.charAt(j)
-        }
-        
-        if ((output != "Rock") && (output != "Paper") && (output != "Scissor"))
-        {
-            return console.log ("Invalid Input");
-        }
-
-        const playerSelection = output;
-        console.log (playRound(playerSelection, computerSelection));
+        output += low.charAt(j)
+    }
+    
+    if ((output != "Rock") && (output != "Paper") && (output != "Scissor"))
+    {
+        return console.log ("Invalid Input");
     }
 
-    if (computer == player)
-        console.log("Tie!")
-    else if (computer > player)
-        console.log("You Lost!")
-    else if (player > computer)
-        console.log("You Won!")
-    else
-        console.log("No one Won!")
+    const playerSelection = output;
+    playRound(playerSelection, computerSelection);
 }
 
-game();
+
+/* if (computer == player)
+console.log("Tie!");
+else if (computer > player)
+console.log("You Lost!");
+else if (player > computer)
+console.log("You Won!");
+else
+console.log("No one Won!"); */
